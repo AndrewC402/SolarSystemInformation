cb2a1ed8137906851342a648148a2b20abffb6b1
Andrew <andrewc.london2019a@gmail.com>
2020-01-14 09:57:25 +0000

    first test written, no code in CUT written

---
+++ SolarSystemInformation - TDD assignment/src/test/java/qa/com/solarSystemInformation/SolarSystemInformationTest.java
@@ -0,0 +1,25 @@
+package qa.com.solarSystemInformation;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolarSystemInformationTest {
+
+    private SolarSystemInformation cut;
+
+    @Test
+    void userID_returned_when_valid_data_is_input () {
+        //arrange
+        String validUserID = "AB1234";
+        cut = new SolarSystemInformation(validUserID);
+        String expectedResult = "AB1234";
+
+        //act
+        String actualResult = cut.getUserID();
+
+        //assert
+        assertEquals(expectedResult,actualResult);
+
+
+    }
+}
